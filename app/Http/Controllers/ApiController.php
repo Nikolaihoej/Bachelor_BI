459f278a60bd399959b446c8adfd904c8f365787
Jacob Juhl <jacobjuhlprivat@gmail.com>
2024-12-04 11:53:42 +0100

    upload af customer data

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
+use App\Models\Customer;
 
 class ApiController extends Controller
 {
     public function index()
     {
-        return response("hej");
+        // return response("hej");
+        $Customers = Customer::all();
+        return response()->json($Customers);
     }
 }
