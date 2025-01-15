caa201da6ffc00348ae6e6a4ae53fd7a2f474c93
Jacob Juhl <jacobjuhlprivat@gmail.com>
2025-01-15 11:32:48 +0100

    added loggin

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,11 +3,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Dashboard;
+use Illuminate\Support\Facades\Log;
 
 class DashboardController extends Controller
 {
     public function store(Request $request)
     {
+        // Log the incoming request data
+        Log::info('Storing new dashboard', ['data' => $request->data]);
+
         $request->validate([
             'data' => 'required|json',
         ]);
@@ -15,6 +19,9 @@ class DashboardController extends Controller
         $dashboard = new Dashboard();
         $dashboard->data = $request->data;
         $dashboard->save();
+
+        // Log the successful storage
+        Log::info('Dashboard stored successfully', ['dashboard_id' => $dashboard->id]);
 
         return response()->json(['message' => 'Dashboard saved successfully'], 201);
     }
