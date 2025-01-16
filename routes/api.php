ec6f45f8a47c6093cbf340c1c260ead966c4d22b
Jacob Juhl <jacobjuhlprivat@gmail.com>
2025-01-16 11:21:53 +0100

    clean up in apis

--- routes/api.php
+++ routes/api.php
@@ -16,27 +16,11 @@ use App\Http\Controllers\DashboardController;
 Route::middleware(JwtMiddleware::class, 'api')->group(function () {
     Route::get('/api/all', [ApiController::class, 'all']);
 
-    Route::post('/api/dashboard', [DashboardController::class, 'store']);
-    Route::get('/api/getdashboards', [DashboardController::class, 'index']);
-    Route::get('/api/getdashboard/{id}', [DashboardController::class, 'getOne']);
-    Route::put('/api/dashboard/{id}/title', [DashboardController::class, 'updateTitle']);
-    Route::delete('/api/dashboard/{id}', [DashboardController::class, 'destroy']);
-
-    Route::get('/api/customers', [ApiController::class, 'customers']);
-    Route::get('/api/customerid', [ApiController::class, 'customerID']);
-    Route::get('/api/customername', [ApiController::class, 'customerName']);
-    Route::get('/api/customeraddress', [ApiController::class, 'customerAddress']);
-    Route::get('/api/customerage', [ApiController::class, 'customerAge']);
-    Route::get('/api/signupdate', [ApiController::class, 'signupDate']);
-    Route::get('/api/customersactivitystatus', [ApiController::class, 'customersActivityStatus']);
-    Route::get('/api/activitystatusid', [ApiController::class, 'activityStatusID']);
-    Route::get('/api/membersincemonths', [ApiController::class, 'memberSinceMonths']);
-    Route::get('/api/hastrainedlastmonth', [ApiController::class, 'hasTrainedLastMonth']);
-    Route::get('/api/dayssincelastvisit', [ApiController::class, 'daysSinceLastVisit']);
-    Route::get('/api/trainingsessionsthismonth', [ApiController::class, 'trainingSessionsThisMonth']);
-    Route::get('/api/membershiptype', [ApiController::class, 'membershipType']);
-    Route::get('/api/membershiptypeid', [ApiController::class, 'membershipTypeID']);
-    Route::get('/api/typename', [ApiController::class, 'typeName']);
+    Route::post('/api/dashboard', [DashboardController::class, 'store']); //Create a new dashboard
+    Route::get('/api/getdashboards', [DashboardController::class, 'index']); //Read all dashboards
+    Route::get('/api/getdashboard/{id}', [DashboardController::class, 'getOne']); //Read one dashboards
+    Route::put('/api/dashboard/{id}/title', [DashboardController::class, 'updateTitle']); //Update the title of a dashboard
+    Route::delete('/api/dashboard/{id}', [DashboardController::class, 'destroy']); //Delete a dashboard
 });
 
 Route::fallback(function () {
